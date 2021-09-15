b6b120a091ce3794c2f3b8af60e72c1d7feb1705
Garrett Smith <garrettarhsmith@gmail.com>
2021-09-15 13:48:23 -0400

    reserve page is partially complete. mostly just missing responsiveness

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
-import React, { useContext, useEffect, useState } from "react";
-import "./App.css";
-import Auth from "./pages/Auth/Auth";
-import Booking from "./pages/Booking/Booking";
-import styled from "styled-components";
-import { UserContext } from "./context/UserProvider";
-import firebase from "firebase/app";
-import "firebase/auth";
+import React, { useContext, useEffect, useState } from 'react';
+import './App.css';
+import Auth from './pages/Auth/Auth';
+import Booking from './pages/Booking/Booking';
+import styled from 'styled-components';
+import { UserContext } from './context/UserProvider';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import Reserve from './pages/Reserve/Reserve';
 
 const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    display: grid;
-    place-items: center;
-    color: whitesmoke;
+  width: 100vw;
+  height: 100vh;
+  oveflow-x: hidden;
+  display: grid;
+  place-items: center;
+  color: whitesmoke;
 `;
 
 function App() {
-    const { user, setUser, signOut } = useContext(UserContext);
-    const [isSignedIn, setIsSignedIn] = useState(
-        JSON.parse(localStorage.getItem("user"))
-    );
+  const { user, setUser, signOut } = useContext(UserContext);
+  const [isSignedIn, setIsSignedIn] = useState(
+    JSON.parse(localStorage.getItem('user'))
+  );
 
-    useEffect(() => {
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                setUser(user);
-                localStorage.setItem("user", true);
-                setIsSignedIn(true);
-            } else {
-                localStorage.removeItem("user");
-                setIsSignedIn(false);
-            }
-        });
-    }, []);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        localStorage.setItem('user', true);
+        setIsSignedIn(true);
+      } else {
+        localStorage.removeItem('user');
+        setIsSignedIn(false);
+      }
+    });
+  }, []);
 
-    return (
-        <Container>
-            {!isSignedIn ? <Auth /> : <Booking signOut={signOut} />}
-            {/* <span>
+  return (
+    <Container>
+      {/* <Booking signOut={signOut} /> */}
+      <Reserve />
+      {/* <span>
                     <h2>You are logged in as</h2>
                     <h1 style={{ color: "#ccc" }}>{user?.email}!</h1>
                     <button
@@ -55,8 +58,8 @@ function App() {
                         Sign Out
                     </button>
                 </span> */}
-        </Container>
-    );
+    </Container>
+  );
 }
 
 export default App;
--- src/pages/Booking/Booking.js
+++ src/pages/Booking/Booking.js
@@ -1,147 +1,113 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-    AppBar,
-    Logo,
-    Container,
-    Content,
-    Column,
-    Text,
-    Bold,
-    RightAligned,
-    Divider,
-    IconBtn,
-    FinalInfo,
-    InfoLine,
-    SubmitBtn,
-} from "./BookingStyling";
-import User from "../../components/User/User";
-import ImgSlider from "../../components/ImgSlider/ImgSlider";
-import { BsPlusCircleFill } from "react-icons/bs";
-import "react-date-range/dist/styles.css"; // main style file
-import "react-date-range/dist/theme/default.css"; // theme css file
-import { DateRange } from "react-date-range";
+  AppBar,
+  Logo,
+  Container,
+  Content,
+  Column,
+  Text,
+  Bold,
+  RightAligned,
+  Divider,
+  IconBtn,
+  FinalInfo,
+  InfoLine,
+  SubmitBtn,
+} from './BookingStyling';
+import User from '../../components/User/User';
+import ImgSlider from '../../components/ImgSlider/ImgSlider';
+import { BsPlusCircleFill } from 'react-icons/bs';
+import format from 'date-fns/format';
 
 function Booking(props) {
-    const [range, setRange] = useState([
-        {
-            startDate: new Date(),
-            endDate: null,
-            key: "selection",
-        },
-    ]);
+  const [dateRange, setDateRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection',
+    },
+  ]);
 
-    function handleChange(item) {
-        setRange([item.selection]);
-        console.log(item);
-    }
-
-    return (
-        <>
-            <AppBar>
-                <Logo>O-D</Logo>
-                <User onClick={props.signOut}></User>
-            </AppBar>
-            <Container>
-                <ImgSlider />
-                <Content>
-                    <Column>
-                        <Text as="h1">1975 Piper Arrow</Text>
-                        <Text as="h4" color="#E35656">
-                            *50 to 100 hour blocks are discounted
-                        </Text>
-                        <Text as="h4" color="#E35656">
-                            *Overnight flights available 1 week in advance
-                        </Text>
-                        <Text as="h3" marginTop="3rem">
-                            Description
-                        </Text>
-                        <Text>
-                            This is a beautiful 1975 Piper Arrow II 200 hp
-                            retractable complex plane. You can rent this plane
-                            out for business, instructing flight lessons or
-                            clocking your pilot hours day or night. You can also
-                            take the plane out for pleasure and enjoy a weekend
-                            up in Jackson Hole or a night down in St. George.
-                            The Sky is the limit.
-                        </Text>
-                        <Text as="h3" color="#E35656" marginTop="3rem">
-                            *Important*
-                        </Text>
-                        <Text as="h4" color="#E35656">
-                            $10 deposit due at checkout. Hourly rate is due upon
-                            returning the plane. Total cost is only an estimate,
-                            you will only be charged for time that the plane is
-                            actually in the air.
-                        </Text>
-                    </Column>
-                    <Column>
-                        <Text as="h3">Choose a date range:</Text>
-                        <DateRange
-                            editableDateInputs={true}
-                            onChange={handleChange}
-                            moveRangeOnFirstSelection={false}
-                            ranges={range}
-                        />
-                        {/* <Text as="h3">Start:</Text>
-                        <div
-                            style={{
-                                width: "300px",
-                                height: "180px",
-                                border: "1px solid gray",
-                            }}
-                        ></div>
-                        <Text as="h3">End:</Text>
-                        <div
-                            style={{
-                                width: "300px",
-                                height: "180px",
-                                border: "1px solid gray",
-                            }}
-                        ></div> */}
-                    </Column>
-                    <Column>
-                        <RightAligned>
-                            <Text as="h1" color="#E35656">
-                                $175 / hr
-                            </Text>
-                            <Text as="p">
-                                <Bold>Start: </Bold>
-                                September 16, 2021 10am
-                            </Text>
-                            <Text as="p">
-                                <Bold>End: </Bold>
-                                September 18, 2021 4pm
-                            </Text>
-                            <Divider />
-                            <IconBtn>
-                                Add another date
-                                <BsPlusCircleFill />
-                            </IconBtn>
-                        </RightAligned>
-                        <FinalInfo>
-                            <InfoLine>
-                                <Text as="h4">Total Hours:</Text>
-                                <Text>29hrs</Text>
-                            </InfoLine>
-                            <InfoLine>
-                                <Text as="h4">Amount due now:</Text>
-                                <Text>$10</Text>
-                            </InfoLine>
-                            <InfoLine>
-                                <Text as="h4">Total Booking Amount:</Text>
-                                <Text>$5075</Text>
-                            </InfoLine>
-                        </FinalInfo>
-                        <Text as="h4" color="#E35656" marginTop="2rem">
-                            Before checking out you will be required to upload a
-                            copy of your pilot's license and insurance
-                        </Text>
-                        <SubmitBtn>Continue</SubmitBtn>
-                    </Column>
-                </Content>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <AppBar>
+        <Logo>O-D</Logo>
+        <User onClick={props.signOut}></User>
+      </AppBar>
+      <Container>
+        <ImgSlider />
+        <Content>
+          <Column>
+            <Text as="h1">1975 Piper Arrow</Text>
+            <Text as="h4" color="#E57879">
+              *50 to 100 hour blocks are discounted
+            </Text>
+            <Text as="h4" color="#E57879">
+              *Overnight flights available 1 week in advance
+            </Text>
+            <Text as="h3" marginTop="3rem">
+              Description
+            </Text>
+            <Text>
+              This is a beautiful 1975 Piper Arrow II 200 hp retractable complex
+              plane. You can rent this plane out for business, instructing
+              flight lessons or clocking your pilot hours day or night. You can
+              also take the plane out for pleasure and enjoy a weekend up in
+              Jackson Hole or a night down in St. George. The Sky is the limit.
+            </Text>
+            <Text as="h3" color="#E57879" marginTop="3rem">
+              *Important*
+            </Text>
+            <Text as="h4" color="#E57879">
+              $10 deposit due at checkout. Hourly rate is due upon returning the
+              plane. Total cost is only an estimate, you will only be charged
+              for time that the plane is actually in the air.
+            </Text>
+          </Column>
+          <Column></Column>
+          <Column>
+            <RightAligned>
+              <Text as="h1" color="#E57879">
+                $175 / hr
+              </Text>
+              <Text as="p">
+                <Bold>Start: </Bold>
+                September 16, 2021 10am
+              </Text>
+              <Text as="p">
+                <Bold>End: </Bold>
+                September 18, 2021 4pm
+              </Text>
+              <Divider />
+              <IconBtn>
+                Add another date
+                <BsPlusCircleFill />
+              </IconBtn>
+            </RightAligned>
+            <FinalInfo>
+              <InfoLine>
+                <Text as="h4">Total Hours:</Text>
+                <Text>29hrs</Text>
+              </InfoLine>
+              <InfoLine>
+                <Text as="h4">Amount due now:</Text>
+                <Text>$10</Text>
+              </InfoLine>
+              <InfoLine>
+                <Text as="h4">Total Booking Amount:</Text>
+                <Text>$5075</Text>
+              </InfoLine>
+            </FinalInfo>
+            <Text as="h4" color="#E57879" marginTop="2rem">
+              Before checking out you will be required to upload a copy of your
+              pilot's license and insurance
+            </Text>
+            <SubmitBtn>Continue</SubmitBtn>
+          </Column>
+        </Content>
+      </Container>
+    </>
+  );
 }
 
 export default Booking;
